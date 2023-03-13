35e2d6776d22eb21e887195ac4fbbf08b6e815c1
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2023-03-13 08:16:27 +0500

    new

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+function resetSubmenuStyles() {
+    let submenus = document.querySelectorAll('.submenu div')
+    for (let submenu of submenus) {
+      submenu.style.width = ''
+      submenu.style.visibility = ''
+    }
+}
+  
+document.querySelector('#menuOne').addEventListener('click', e => {
+    let submenu = document.querySelector('.submenu')
+    let submenuOne = document.querySelector('#submenuOne')
+    let submenuStyle = window.getComputedStyle(submenuOne)
+    let submenuWidth = parseInt(submenuStyle.getPropertyValue('width')) - parseInt(submenuStyle.getPropertyValue('padding-left')) - parseInt(submenuStyle.getPropertyValue('padding-right'))
+    
+    resetSubmenuStyles()
+    if (submenuWidth == 0) {
+      submenu.style.width = '20%'
+      submenuOne.style.width = '100%'
+      submenuOne.style.visibility = 'visible'
+    }
+    else {
+      submenu.style.width = ''
+      submenuOne.style.width = ''
+      submenuOne.style.visibility = ''
+    }
+})
+  
+document.querySelector('#menuTwo').addEventListener('click', e => {
+    let submenu = document.querySelector('.submenu')
+    let submenuTwo = document.querySelector('#submenuTwo')
+    let submenuStyle = window.getComputedStyle(submenuTwo)
+    let submenuWidth = parseInt(submenuStyle.getPropertyValue('width')) - parseInt(submenuStyle.getPropertyValue('padding-left')) - parseInt(submenuStyle.getPropertyValue('padding-right'))
+    resetSubmenuStyles()
+    if (submenuWidth == 0) {
+      submenu.style.width = '20%'
+      submenuTwo.style.width = '100%'
+      submenuTwo.style.visibility = 'visible'
+    }
+    else {
+      submenu.style.width = ''
+      submenuTwo.style.width = ''
+      submenuTwo.style.visibility = ''
+    }
+})
